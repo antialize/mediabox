00ea4900906e26ab6df648205f13d76f5e4b2b5e
Jakob Truelsen <jakobt@lain.(none)>
2008-12-31 20:16:25 +0100

    Apply the gpl

---
+++ autogpl.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Mediabox: a light mediacenter solution
+# Copyright (C) 2009 Jakob Truelsen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+if [ ! -f gpl-3.0.txt ]; then
+	wget -O gpl-3.0.txt http://www.gnu.org/licenses/gpl-3.0.txt
+fi
+
+for x in *.cc *.hh; do
+	if ! grep -q "WITHOUT ANY WARRANTY" $x; then
+		cat - $x > $x~ <<EOF
+/*
+ * Mediabox: a light mediacenter solution
+ * Copyright (C) 2009 Jakob Truelsen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+EOF
+		mv $x~ $x
+	fi
+done
